6377c36b9026d93780778c0a4bf1fed12d61e3dc
Eoin Kavanagh <ekavanagh@emoneyadvisor.com>
2021-09-09 19:17:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiscoCUIC_XMPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
